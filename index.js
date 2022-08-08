7aba5be57edadabc26db42684b2dcd08243490c0
Dan Muendo <danwalker254254@gmail.com>
2022-08-08 09:41:18 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.querySelector("h1#victory");
+const newElement = document.createElement("div");
+
+newElement.textContent = ("YOUR-NAME is the champion");
+newHeader.appendChild(newElement);
+document.body.append(newElement);
